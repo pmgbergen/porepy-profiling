712b56c48101d756aa1e8f0f2859b18c31b61e57
Yuri Zabegaev <yuri.zabegaev@uib.no>
2024-12-21 19:27:39 +0100

    Updates in benchmarks

--- job.sh
+++ job.sh
@@ -12,9 +12,17 @@ echo "Starting asv profiling"
 echo "Generating html report"
 /usr/local/bin/asv publish
 
-echo "Publishing updates on github"
-git add .asv
-git commit -m "Profiling update"
-git push origin main
+# Folder to check
+FOLDER_TO_CHECK=".asv/"
+
+# Check for changes
+if git status --porcelain | grep -q -e "^ M  $FOLDER_TO_CHECK" -e "^?? $FOLDER_TO_CHECK"; then
+    echo "Publishing updates on github"
+    git add .asv
+    git commit -m "Profiling update"
+    git push origin main
+else
+    echo "No changes in $FOLDER_TO_CHECK."
+fi
 
 echo "Job completed successfully"
