6a3f37558d796ea1ae701f127c7b2aeb5e770e4e
Yuri Zabegaev <yuri.zabegaev@uib.no>
2025-01-09 15:24:35 +0100

    Updated crontab

--- crontab.sh
+++ crontab.sh
@@ -1,2 +1,2 @@
 # This runs the script job.sh as a cronjob every hour and redirects its log, both stdout and stderr.
-0 * * * * sh /root/app/job.sh >> /var/log/cron.log 2>&1
+0 0 * * * sh /root/app/job.sh >> /var/log/cron.log 2>&1
